e4b22ddc3dd8bf6bb1da429750ff0134aa7347d1
sushmapyalla <priyadarsini.sushma@gmail.com>
2023-11-23 07:34:04 +0530

    madel training with hyper parameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
    train_data_path: str =os.path.join('artifacts',"train.csv") 
@@ -40,7 +43,7 @@ class DataIngestion:
 
          logging.info('Ingestion of the data completed')
 
-         return (
+         return(
 
             self.Ingestion_config.train_data_path,
             self.Ingestion_config.test_data_path
@@ -55,8 +58,10 @@ if __name__=='__main__':
    train_data,test_data=obj.initiate_data_ingestion()
 
    data_tranformation = DataTransformation()
-   DataTransformation.initiate_data_transformation(train_data, test_data)
+   train_arr,test_arr,_=data_tranformation.initiate_data_transformation(train_data, test_data)
    
+   modeltrainer=ModelTrainer()
+   print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
    
 
          
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -42,7 +42,7 @@ class DataTransformation:
 
                     ("imputer",SimpleImputer(strategy= "median")),
 
-                    ("scaler", StandardScaler())
+                    ("scaler", StandardScaler(with_mean=False))
                 ]
 
             )
@@ -53,7 +53,7 @@ class DataTransformation:
 
                     ('imputer',SimpleImputer(strategy="most_frequent")),
                     ('one hot encoder', OneHotEncoder()),
-                    ('scaler', StandardScaler())
+                    ('scaler', StandardScaler(with_mean=False))
                 ]
             )
 
@@ -85,8 +85,8 @@ class DataTransformation:
             logging.info ('Read train and test data completed')
             logging.info('obtaining preprocessing object')
 
+            # preprocessing_obj = self.get_data_transformer_object()
             preprocessing_obj = self.get_data_transformer_object()
-
             target_column_name="math_score"
             numerical_columns = ['writing_score','reading_score']
 
@@ -97,7 +97,7 @@ class DataTransformation:
 
             target_feature_train_df= train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(coulmns=[target_column_name],axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
 
             target_feature_test_df= test_df [target_column_name]
 
@@ -125,12 +125,50 @@ class DataTransformation:
               obj = preprocessing_obj
 
             )
+            # target_column_name="math_score"
+            # numerical_columns = ['writing_score','reading_score']
+
+            # categorical_columns =["gender","race_ethnicity","parental_level_of_education",
+            #                       "lunch","test_preparation_course"]
+            
+            # input_feature_train_df= train_df.drop(columns=[target_column_name],axis=1)
+
+            # target_feature_train_df= train_df[target_column_name]
+
+            # input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+
+            # target_feature_test_df= test_df[target_column_name]
+
+            # logging.info ("Applying processing object on training and testing data frame")
+
+            # input_feature_train_arr= preprocessing_obj.fit_transform(input_feature_train_df)
+
+            # input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            # train_arr = np.c_[
+            #     input_feature_train_arr, np.array(target_feature_train_df)
+            # ]
+
+            # test_arr =np.c_[
+
+            #     input_feature_test_arr,np.array(target_feature_test_df)
+            # ]
+
+            # logging.info(f"saved preprocessing object.")
+
+            # save_object(
+
+            #   file_path = self.data_transformation_config.preprocessor_obj_file_path,
+
+            #   obj = preprocessing_obj
+
+            # )
 
             return (
 
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
             raise CustomException (e,sys)
